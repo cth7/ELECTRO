0a3e8ef6d0320ab1caffd025edf808b2411cea21
Chris Huynh <cthuynh91@gmail.com>
2026-09-18 03:45:51 +0000

    Switch order of arguments to better reflect importance

--- src/electro/points.py
+++ src/electro/points.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def square(point, num_points, n0=0, offset=0, dtype=np.float64):
+def square(point, num_points, offset=0, n0=0, dtype=np.float64):
     """Generates an incremental sequence of points that wrap the unit square.
 
     Args:
         point: The sequence is incremented by this value.
         num_points: Total number of points to generate in this sequence.
+        offset: The sequence is shifted by this value.
         n0: By default the sequence starts with n=0, 1, 2,... Change this
             integer to start the sequence at a later point. For example, use 2
             to start with n=2, 3, 4,...
-        offset: The sequence is shifted by this value.
 
     Returns:
         out: Array with shape (num_points, len(point)).
